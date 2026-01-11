8befbe9157905612311970e2923fcd02ac685005
Ghiath <ghiatham86@gmail.com>
2026-01-11 04:33:42 +0100

    حذف الملفات القديمة استعدادًا للنشر

--- server/prisma/migrations/20260105133948_make_starter_player_nullable/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `winner_team` on the `Match` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Match" DROP COLUMN "winner_team",
-ADD COLUMN     "winner_team_id" INTEGER;
