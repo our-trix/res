a44ca2630d7f101f3c19642fa5ffa58d81d7bda3
Ghiath <ghiatham86@gmail.com>
2026-01-12 01:46:39 +0100

    clean gh-pages

--- server/prisma/migrations/20260105104613_make_starter_player_nullable/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `starter_player` on the `Match` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Match" DROP COLUMN "starter_player",
-ADD COLUMN     "starter_player_id" INTEGER;
